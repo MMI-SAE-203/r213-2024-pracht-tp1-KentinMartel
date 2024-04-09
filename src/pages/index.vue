dea47fa93e28990db40e3c5ba81c7634dcdf457c
KentinMartel <kentin.martel@edu.univ-fcomte.fr>
2024-04-09 14:25:04 +0200

    cache  / pas cache

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-const maVar = true
+import { ref } from 'vue';
+
+const maVar = ref(true)
 </script>
 
 <template>
+  <button @click="maVar = false">cache</button>
+  <br> 
+  <button @click="maVar = true">pas cache</button>
+
   <h1 v-show="maVar" class="text-2xl">Bonjour monde !</h1>
 </template>
