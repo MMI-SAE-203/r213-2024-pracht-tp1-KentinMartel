9327d05f313b4cedfd6091b3bc4a26295b5b993b
KentinMartel <kentin.martel@edu.univ-fcomte.fr>
2024-04-12 10:57:12 +0200

    ooo

--- src/pages/boucle.vue
+++ src/pages/boucle.vue
@@ -5,27 +5,15 @@ import { ref } from 'vue'
 const sectionsData = [
   {
     label: 'bouton 1',
-    texte: `texte panneau 1 Lorem ipsum dolor sit ame
-    consectetur adipisicing elit. Inventore animi dolore,
-    rerum magni laudantium quod excepturi laboriosam, 
-    ex modi debitis harum reprehenderit eaque quam ut
-    ea molestiae. Id, cum dolor!`
+    texte: `Bouton uno`
   },
   {
     label: 'bouton 2',
-    texte: `texte panneau 2 Amet alias provident quos quis, 
-    Officia ut ab dolores quos dolorem accusamus ad,
-    consectetur unde minima, ipsum eligendi inventore id
-    labore, laborum rerum laboriosam corrupti iste.
-    Distinctio, perspiciatis!`
+    texte: `Ceci est le bouton 2 !`
   },
   {
     label: 'bouton 3',
-    texte: `texte panneau 3 Repudiandae corporis voluptates, 
-    odit reprehenderit sint pariatur at voluptatum, cumque
-    quia sit eligendi ex culpa eos, alias magnam molestiae
-    id modi accusantium ipsa eveniet accusamus. Tempora 
-    quis corporis et nam.`
+    texte: `Ceci est le bouton 3 !!`
   }
 ]
 const sectionOpen = ref(50)
