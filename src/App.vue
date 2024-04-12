83df312672d24da217029c13ea43678dced4d9a1
KentinMartel <kentin.martel@edu.univ-fcomte.fr>
2024-04-12 11:11:48 +0200

    background color

--- src/App.vue
+++ src/App.vue
@@ -12,43 +12,44 @@ const menuIsOpen = ref(false)
 </script>
 
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li>
-          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-        </li>
-      </ul>
+  <div class="bg-red-300 h-dvh">
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <RouterView v-slot="{ Component }">
+      <Suspense>
+        <component :is="Component" />
+      </Suspense>
+    </RouterView>
+    
+    <button @pointerdown="menuIsOpen = !menuIsOpen"
+    aria-controls="mainNav"
+    aria-expanded="true"
+    class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+    >
+    menu
+    </button>
+    <!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
+    <Transition
+    class="transition-transform duration-500"
+    enter-from-class="-translate-x-full"
+    enter-to-class="translate-x-0"
+    leave-active-class="-translate-x-full"
+    >
+    <nav v-show="menuIsOpen" id="mainNav">
+    <ul>
+      <li><a href="#">item 1</a></li>
+      <li><a href="#">item 2</a></li>
+      <li><a href="#">item 3</a></li>
+    </ul>
     </nav>
-  </header>
-  <RouterView v-slot="{ Component }">
-    <Suspense>
-      <component :is="Component" />
-    </Suspense>
-  </RouterView>
-
-<button @pointerdown="menuIsOpen = !menuIsOpen"
-  aria-controls="mainNav"
-  aria-expanded="true"
-  class="rounded-full border-2 border-red-600 bg-red-300 px-2"
->
-  menu
-</button>
-<!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
-<Transition
-  class="transition-transform duration-500"
-  enter-from-class="-translate-x-full"
-  enter-to-class="translate-x-0"
-  leave-active-class="-translate-x-full"
->
-<nav v-show="menuIsOpen" id="mainNav">
-
-  <ul>
-    <li><a href="#">item 1</a></li>
-    <li><a href="#">item 2</a></li>
-    <li><a href="#">item 3</a></li>
-  </ul>
-</nav>
-</Transition>
+    </Transition>
+  </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
-  },
-  plugins: []
+    extend: {
+      
+    }
+  }
 }
